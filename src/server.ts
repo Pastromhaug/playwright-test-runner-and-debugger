3f81442d9f00fbfe61e8baf473a50afca65d7aba
Per Andre Stromhaug <perandrestromhaug@gmail.com>
2025-05-11 18:17:37 +0200

    forgot to tsc

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,6 @@ import fs from "fs";
 import yargs from "yargs";
 import { z } from "zod";
 
-import { add } from "./add.js";
-
 const argv = yargs(process.argv.slice(2))
   .options({
     "project-root-path": {
@@ -203,22 +201,22 @@ server.addTool({
   }),
 });
 
-server.addTool({
-  annotations: {
-    openWorldHint: false, // This tool doesn't interact with external systems
-    readOnlyHint: true, // This tool doesn't modify anything
-    title: "Addition",
-  },
-  description: "Add two numbers",
-  execute: async (args) => {
-    return String(add(args.a, args.b));
-  },
-  name: "add",
-  parameters: z.object({
-    a: z.number().describe("The first number"),
-    b: z.number().describe("The second number"),
-  }),
-});
+// server.addTool({
+//   annotations: {
+//     openWorldHint: false, // This tool doesn't interact with external systems
+//     readOnlyHint: true, // This tool doesn't modify anything
+//     title: "Addition",
+//   },
+//   description: "Add two numbers",
+//   execute: async (args) => {
+//     return String(add(args.a, args.b));
+//   },
+//   name: "add",
+//   parameters: z.object({
+//     a: z.number().describe("The first number"),
+//     b: z.number().describe("The second number"),
+//   }),
+// });
 
 server.addPrompt({
   arguments: [
